9a1a11de238dde5587b3ff169795f990bf5fa622
trinhxuandat2003 <hoatrinh3000@gmail.com>
2023-04-28 23:06:38 +0700

    Thay đổi bố cục reset password

--- login/reset-password.php
+++ login/reset-password.php
@@ -4,154 +4,46 @@ session_start();
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&family=Poppins:wght@400;500;600&display=swap');
 
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: "Poppins", sans-serif;
-    }
-
     body {
-        margin: 0;
-        padding: 0;
-        background: #FFF8D9;
-        height: 100vh;
-        overflow: hidden;
-    }
-
-    .center {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 400px;
-        background: white;
-        border-radius: 10px;
-        box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.05);
-    }
-
-    .center h1 {
-        text-align: center;
-        padding: 20px 0;
-        border-bottom: 1px solid silver;
-    }
-
-    .center form {
-        padding: 0 40px;
-        box-sizing: border-box;
-    }
-
-    form .txt_field {
-        position: relative;
-        border-bottom: 2px solid #adadad;
-        margin: 30px 0;
-    }
-
-    .txt_field input {
-        width: 100%;
-        padding: 0 5px;
-        height: 40px;
-        font-size: 16px;
-        border: none;
-        background: none;
-        outline: none;
-    }
-
-    .txt_field label {
-        position: absolute;
-        top: 50%;
-        left: 5px;
-        color: #adadad;
-        transform: translateY(-50%);
-        font-size: 16px;
-        pointer-events: none;
-        transition: .5s;
-    }
-
-    .txt_field span::before {
-        content: '';
-        position: absolute;
-        top: 40px;
-        left: 0;
-        width: 0%;
-        height: 2px;
-        background: #2691d9;
-        transition: .5s;
-    }
-
-    .txt_field input:focus~label,
-    .txt_field input:valid~label {
-        top: -5px;
-        color: #2691d9;
-    }
-
-    .txt_field input:focus~span::before,
-    .txt_field input:valid~span::before {
-        width: 100%;
-    }
-
-    .pass {
-        margin: -5px 0 20px 5px;
-        color: #a6a6a6;
-        cursor: pointer;
-    }
-
-    .pass:hover {
-        text-decoration: underline;
-    }
-
-    input[type="submit"] {
-        width: 100%;
-        height: 50px;
-        border: 1px solid;
-        background: #2691d9;
-        border-radius: 25px;
-        font-size: 18px;
-        color: #e9f4fb;
-        font-weight: 700;
-        cursor: pointer;
-        outline: none;
-    }
-
-    input[type="submit"]:hover {
-        border-color: #2691d9;
-        transition: .5s;
-    }
-
-    .signup_link {
-        margin: 30px 0;
-        text-align: center;
-        font-size: 16px;
-        color: #666666;
-    }
-
-    .signup_link a {
-        color: #2691d9;
-        text-decoration: none;
-    }
-
-    .signup_link a:hover {
-        text-decoration: underline;
-    }
+    text-align: center;
+    background: #fff8d9;
+}
+form {
+    display: inline-block;
+}
+.center{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  background: white;
+  border-radius: 10px;
+  box-shadow: 10px 10px 15px rgba(0,0,0,0.05);
+}
+.center .con{
+    position: relative;
+}
 </style>
 <main>
-    <div class="wrapper-main">
-        <section class="section-default">
-            <h1>Reset your password</h1>
-            <p>An email will be sent to you with instructions on how to reset your password</p>
+    <body>
+        <div class="center">
             <form action="includes/reset-request.inc.php" method="post">
-                <input type="text" name="email" placeholder="Enter your email address">
-                <button type="submit" name="reset-request-submit">Receive new password by email</button>
-
-            </form>
-
-            <?php
+            
+             <h1>Reset your password</h1>
+             <p>An email will be sent to you with instructions on how to reset your password</p>
+                <div class="con">
+                    <input type="email" name= "email" placeholder="Enter your email address"> <br>
+                </div>  
+               <button type="submit" name="reset-request-submit">Receive new password by email</button>
+               <?php
             if (isset($_GET['reset'])) {
                 if ($_GET['reset'] == "success") {
                     echo '<p>Check your email</p>';
                 }
             }
             ?>
-        </section>
-    </div>
+            </form>
+        </div>
+    </body>
 </main>
